777ec7377719bf3dc01d92ba35fe625ca359c4f4
raizengxd <rickdavidsangalang@gmail.com>
2020-06-22 18:34:07 +0800

    Updates

---
+++ precache-manifest.2d75baac8c5e9b25e6d6def98e93bd67.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4f52a85f17782e21abeb7bdfe2d1155c",
+    "url": "/raizengxdweb/index.html"
+  },
+  {
+    "revision": "dd7a10186ead6d76ff4a",
+    "url": "/raizengxdweb/static/css/main.0b301350.chunk.css"
+  },
+  {
+    "revision": "f5f6970ac06bb910f383",
+    "url": "/raizengxdweb/static/js/2.42985a5a.chunk.js"
+  },
+  {
+    "revision": "d36ba6caeaf42b0af54f7780c08f78f4",
+    "url": "/raizengxdweb/static/js/2.42985a5a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dd7a10186ead6d76ff4a",
+    "url": "/raizengxdweb/static/js/main.90f6f2a8.chunk.js"
+  },
+  {
+    "revision": "57fe937ee4bbed634527",
+    "url": "/raizengxdweb/static/js/runtime-main.529a0bf2.js"
+  },
+  {
+    "revision": "78f41fe3c5da8d25e565d518a149651a",
+    "url": "/raizengxdweb/static/media/Slide1.78f41fe3.PNG"
+  },
+  {
+    "revision": "b180e8b80135caee0c67d48baa5e8023",
+    "url": "/raizengxdweb/static/media/Slide1.b180e8b8.PNG"
+  },
+  {
+    "revision": "9dd72e4567ff514708ebe65e8e0644ca",
+    "url": "/raizengxdweb/static/media/image-2.9dd72e45.jpg"
+  },
+  {
+    "revision": "17b969f2f6806c67d5c3a3946751c694",
+    "url": "/raizengxdweb/static/media/logo-cca.17b969f2.png"
+  },
+  {
+    "revision": "ae91b723cbdab27450b707509964eb4d",
+    "url": "/raizengxdweb/static/media/logo-fc.ae91b723.jpg"
+  },
+  {
+    "revision": "014350657454d640ee85663c9076954c",
+    "url": "/raizengxdweb/static/media/logo-yt.01435065.png"
+  }
+]);
